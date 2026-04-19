766977059fa01b2ce7bcc768e3c03a28218d1c82
Gustavo Amaral Eliseu <gustavoeliseu@gmail.com>
2026-04-19 19:57:37 -0300

    small fixes

--- kmpdexlib-shared/kmpdexlib_shared.podspec
+++ kmpdexlib-shared/kmpdexlib_shared.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
     spec.name                     = 'kmpdexlib_shared'
-    spec.version                  = '0.1.0'
+    spec.version                  = '0.2.1'
     spec.homepage                 = 'https://github.com/GustavoEliseu/KMPDexLib'
     spec.source                   = { :http=> ''}
     spec.authors                  = ''
--- kmpdexlib-shared/src/iosMain/kotlin/com/gustavo/kmpdexlib/shared/KMPDexLib.kt
+++ kmpdexlib-shared/src/iosMain/kotlin/com/gustavo/kmpdexlib/shared/KMPDexLib.kt
@@ -7,6 +7,7 @@ import com.gustavo.kmpdexlib.concept.list.di.listModule
 import com.gustavo.kmpdexlib.concept.list.features.list.PokemonListScreen
 import com.gustavo.kmpdexlib.core.data.coreDataModule
 import com.gustavo.kmpdexlib.core.infra.coreInfraModule
+import org.koin.compose.KoinContext
 import org.koin.core.context.startKoin
 import platform.UIKit.UIViewController
 
@@ -31,8 +32,10 @@ fun startKMPDexLib() {
  * @param onPokemonClick called with the selected Pokémon's id when the user taps a card.
  */
 fun pokemonListViewController(onPokemonClick: (Int) -> Unit): UIViewController =
-    ComposeUIViewController {
-        PokemonListScreen(onPokemonClick = onPokemonClick)
+    ComposeUIViewController(configure = { enforceStrictPlistSanityCheck = false }) {
+        KoinContext {
+            PokemonListScreen(onPokemonClick = onPokemonClick)
+        }
     }
 
 /**
@@ -42,6 +45,8 @@ fun pokemonListViewController(onPokemonClick: (Int) -> Unit): UIViewController =
  * @param onBack called when the user taps the back button.
  */
 fun pokemonDetailViewController(pokemonId: Int, onBack: () -> Unit): UIViewController =
-    ComposeUIViewController {
-        PokemonDetailScreen(pokemonId = pokemonId, onBack = onBack)
+    ComposeUIViewController(configure = { enforceStrictPlistSanityCheck = false }) {
+        KoinContext {
+            PokemonDetailScreen(pokemonId = pokemonId, onBack = onBack)
+        }
     }
