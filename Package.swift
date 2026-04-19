861e7e862fb98e418820ae91fb82b415305373e7
Gustavo Amaral Eliseu <gustavoeliseu@gmail.com>
2026-04-19 18:15:30 -0300

    chore: release v0.1.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "KMPDexLib",
             url: "https://github.com/GustavoEliseu/KMPDexLib/releases/download/v0.1.0/KMPDexLib.xcframework.zip",
-            checksum: "CHECKSUM_PLACEHOLDER"
+            checksum: "9072e5afe955880520d85a6cec0c0f6ca55ecfdc8a3fadb3dd44348036f143d8"
         ),
     ]
 )
